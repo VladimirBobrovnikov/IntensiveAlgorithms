0b57860a5780283409544d726d4f01f574521e47
vladimir <bobrovnikov2vladimir@yandex.ru>
2023-08-18 21:58:51 +0300

    fix

--- PrimeNumbers/prime_numbers.py
+++ PrimeNumbers/prime_numbers.py
@@ -11,16 +11,13 @@ def prime_numbers(n: int) -> List[int]:
     Returns:
         List[int]: prime numbers
     """
-    if n <= 2:
-        return [2]
 
     for_compare = {i for i in range(2, n + 1)}
     for_del = set()
-    for index in range(2, int(n**(0.5)) + 1):
+    for index in range(2, int(n ** (0.5)) + 1):
         for elem in for_compare.difference(for_del):
             if elem > index and elem % index == 0:
                 for_del.add(elem)
     return [elem for elem in for_compare.difference(for_del)]
-
 
 # https://leetcode.com/problems/count-primes/
