820863bd82dd3e8be813872397253891aea50b4e
vladimir <bobrovnikov2vladimir@yandex.ru>
2023-08-18 21:13:15 +0300

    bfs

--- BreadthFirstTreeTraversal/breadth_first_tree_traversal.py
+++ BreadthFirstTreeTraversal/breadth_first_tree_traversal.py
@@ -1,19 +1,47 @@
-from typing import Dict
-from typing import List
+from typing import List, Optional
+from queue import Queue
 
 
-def breadth_first_tree_traversal(root: int, tree: Dict[int, List[int]]) -> List[int]:
-    """
-    Completes breadth first tree traversal and returns visited vertices
-    Go to left side of tree, then right, then self
+class TreeNode:
+    def __str__(self):
+        return f'TreeNode {self.val=},'
 
-    Args:
-        root (int): root vertex
-        tree (Dict[int, List[int]]): structure of tree
-                            (number of vertex, list of two elements: child number or None)
-    """
-
-    return []
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
-# https://leetcode.com/problems/binary-tree-level-order-traversal/
+class Solution:
+    result = []
+    queue = Queue()
+
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        self.result.clear()
+
+        if not root:
+            return []
+
+        temp_ = []
+        self.queue.put((root, 0))
+        lvl_now = 0
+        while True:
+            if self.queue.empty():
+                if temp_:
+                    self.result.append(temp_.copy())
+                break
+            else:
+                noda, lvl = self.queue.get()
+                if lvl == lvl_now:
+                    temp_.append(noda.val)
+                else:
+                    lvl_now = lvl
+                    self.result.append(temp_.copy())
+                    temp_.clear()
+                    temp_.append(noda.val)
+                if noda.left:
+                    self.queue.put((noda.left, lvl + 1))
+                if noda.right:
+                    self.queue.put((noda.right, lvl + 1))
+
+        return self.result
