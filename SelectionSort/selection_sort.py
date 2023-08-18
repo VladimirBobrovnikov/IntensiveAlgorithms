10dd0c0dd301bb99ce3dabe639c040306c8417b6
vladimir <bobrovnikov2vladimir@yandex.ru>
2023-08-18 18:32:53 +0300

    compleate selection_sort

--- SelectionSort/selection_sort.py
+++ SelectionSort/selection_sort.py
@@ -11,8 +11,16 @@ def selection_sort(array: List[int]) -> List[int]:
     Returns:
         List[int]: sorted array
     """
+    for i in range(len(array) - 1):
+        minimum = array[i]
+        index_minimum = None
+        for j in range(i, len(array)):
+            if array[j] < minimum:
+                minimum = array[j]
+                index_minimum = j
 
-    return []
-
+        if index_minimum:
+            array[i], array[index_minimum] = array[index_minimum], array[i]
+    return array
 
 # https://leetcode.com/problems/sort-colors/
