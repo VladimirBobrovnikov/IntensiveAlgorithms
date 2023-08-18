a5c158ca956725c438f1a255cf1a8c52acc11db0
vladimir <bobrovnikov2vladimir@yandex.ru>
2023-08-18 18:21:36 +0300

    bubble_sort compleat

--- BubbleSort/bubble_sort.py
+++ BubbleSort/bubble_sort.py
@@ -11,8 +11,18 @@ def bubble_sort(array: List[int]) -> List[int]:
     Returns:
         List[int]: sorted array
     """
+    flag_action = True
+    for i in range(len(array) - 1):
+        if not flag_action:
+            break
+        else:
+            flag_action = False
 
-    return []
+        for j in range(len(array) - 1 - i):
+            if array[j + 1] < array[j]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                flag_action = True
 
+    return array
 
 # https://leetcode.com/problems/sort-colors/description/
