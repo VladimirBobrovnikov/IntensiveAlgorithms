12fd46f3d9b332802f18f8506a169734954688b8
vladimir <bobrovnikov2vladimir@yandex.ru>
2023-08-18 21:45:01 +0300

    скобки

--- Brackets/brackets.py
+++ Brackets/brackets.py
@@ -1,3 +1,11 @@
+from collections import deque
+
+brackets_mapping = {
+    '{': '}',
+    '[': ']',
+    '(': ')',
+}
+
 def are_brackets_correct(string: str) -> bool:
     """
     Accepts a string consisting only of opening and closing parentheses as input,
@@ -10,8 +18,20 @@ def are_brackets_correct(string: str) -> bool:
     Returns:
         bool: are parentheses correct
     """
+    stack = deque()
+    for elem in string:
+        if brackets_mapping.get(elem):
+            stack.append(elem)
+        else:
+            if not len(stack):
+                return False
+            compare = stack.pop()
+            if elem != brackets_mapping.get(compare):
+                return False
+    if len(stack):
+        return False
 
-    return False
+    return True
 
 
 # https://leetcode.com/problems/valid-parentheses/
